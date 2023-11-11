cb97171bea03eadc28c2ca6198b8511bc6480b26
DavidsCreation <150444074+DavidsCreation@users.noreply.github.com>
2023-11-11 18:50:51 +0000

    watermarks can now be deleted.

--- wrapper/watermark/delete.js
+++ wrapper/watermark/delete.js
@@ -1,11 +1,12 @@
 const formidable = require('formidable');
+const folder = `${__dirname}/../.${process.env.WATERMARKS_FOLDER}`;
 const fs = require('fs');
 
 module.exports = function (req, res, url) {
 	if (req.method != 'POST' || url.path != '/api/watermark/delete') return;
 	new formidable.IncomingForm().parse(req, (e, f) => {
 		try {
-            fs.unlinkSync(`../_WATERMARKS/${f.id}`);
+            fs.unlinkSync(`${folder}/${f.id}`);
             res.end(JSON.stringify({status: "ok"}));
         } catch (e) {
             console.log(e);
